1c0a30ec6b53948d7ff8c469266604e0b24f78e6
Madhu-R-cloud <madhusudanr00@gmail.com>
2023-11-28 14:12:36 +0530

    Remove Duplicate element in a string program is updated

--- removeDuplicateElement.js
+++ removeDuplicateElement.js
@@ -1,18 +1,16 @@
-const readline = require('readline');
-
-const rl = readline.createInterface({
-    input: process.stdin,
-    output: process.stdout
-});
-
+const readline = require('readline-sync');
 function removeDuplicates(str) {
+    let result = '';
 
-    return [...new Set(str)].join('');
-   
+    for (let i = 0; i < str.length; i++) {
+        if (result.indexOf(str[i]) === -1) {
+            result += str[i];
+        }
+    }
+    return result;
 }
 
-rl.question('Enter a string: ', (userInput) => {
-    const result = removeDuplicates(userInput);
+const str1 = readline.question('Enter a string: ',) 
+    const result = removeDuplicates(str1);
     console.log(`String after removing duplicates: ${result}`);
-    rl.close();
-});
+
