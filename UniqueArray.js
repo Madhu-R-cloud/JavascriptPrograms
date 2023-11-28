6297240f8ac18070f7e245b0aec4b99121d72b6b
Madhu-R-cloud <madhusudanr00@gmail.com>
2023-11-28 14:51:06 +0530

    Unique array elements function is updated in a program

--- UniqueArray.js
+++ UniqueArray.js
@@ -1,17 +1,29 @@
+
+const readline = require('readline-sync');
+
+let str1 = readline.question('Enter elements of the array1 (comma-separated): ');
+let str2 = readline.question('Enter elements of the array2 (comma-separated): ');
+const elements1 = str1.split(',').map(Number);
+const elements2 = str2.split(',').map(Number);
+
+let result = mergeUniqueArrays(elements1, elements2);
+console.log(result);
+
 function mergeUniqueArrays(arr1, arr2) {
-   
-    const uniqueSet = new Set([...arr1, ...arr2]);
+    let mergedArray = [];
 
-    const uniqueArray = [...uniqueSet];
+    for (let i = 0; i < arr1.length; i++) {
+        if (!mergedArray.includes(arr1[i])) {
+            mergedArray.push(arr1[i]);
+        }
+    }
 
-    return uniqueArray;
+    for (let j = 0; j < arr2.length; j++) {
+        if (!mergedArray.includes(arr2[j])) {
+            mergedArray.push(arr2[j]);
+        }
+    }
+    return mergedArray;
 }
 
-const array1 = [1, 2, 3, 4];
-const array2 = [3, 4, 5, 6];
 
-const mergedArray = mergeUniqueArrays(array1, array2);
-
-console.log('Array 1 :', array1);
-console.log('Array 2 :', array2);
-console.log('Merged Unique Array :', mergedArray);
