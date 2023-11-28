d47b37d16debacdd352885f289fa45e3f996ee87
Madhu-R-cloud <madhusudanr00@gmail.com>
2023-11-28 14:31:28 +0530

    Square adjacent element swaps in a array function is updated in a program

--- squareAdjacentEleSwap.js
+++ squareAdjacentEleSwap.js
@@ -1,3 +1,14 @@
+var readline = require("readline-sync");
+
+const str1 = readline.question('Enter elements of the array (space-separated):');
+const elements = str1.split(' ').map(Number);
+
+console.log('Original Array:',elements);
+
+const resultArray = squareAndSwap(elements);
+console.log('Array after squaring and swapping adjacent elements:', resultArray);
+
+
 // Function to square adjacent elements and swap them
 function squareAndSwap(arr) {
    
@@ -13,9 +24,3 @@ function squareAndSwap(arr) {
     return arr;
 }
 
-
-const originalArray = [1, 2, 3, 4, 5, 6];
-const resultArray = squareAndSwap(originalArray);
-
-console.log('Original Array:', originalArray);
-console.log('Array after squaring and swapping adjacent elements:', resultArray);
